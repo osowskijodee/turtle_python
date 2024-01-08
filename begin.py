0f9f62add504a1c61ee84b88a8be06086ae14511
Kristy <104734454+osowskijodee@users.noreply.github.com>
2024-01-08 14:15:32 +0300

    Create begin.py

---
+++ begin.py
@@ -0,0 +1,15 @@
+import turtle
+​
+# Create a turtle screen
+screen = turtle.Screen()
+​
+# Create a turtle named "pen"
+pen = turtle.Turtle()
+​
+# Draw a square
+for _ in range(4):
+    pen.forward(100)
+    pen.right(90)
+​
+# Close the turtle graphics window on click
+screen.exitonclick()
